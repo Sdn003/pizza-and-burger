ed7d6f5cdd1f82db5dafcfc13444032b46b45bc9
Sudharshan <s.sudharsan12@tcs.com>
2023-09-23 20:56:45 +0530

    arrow

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,18 @@
 import React, { Fragment, useContext } from "react";
 import { FoodContext } from "../../App";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import pizzaVideo from "../videos/Pizza - Promo Video.mp4";
 import "./Home.css";
 import InfiniteSwiper from "../Swiper/Swiper";
 import about_us_image from "../../components/images/about_us.jpg";
 import about_us_bg from "../../components/images/about_us_bg.jpeg";
+import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 
 import icon_1 from "../../components/images/icon_images/pic_1.jpg";
 import icon_2 from "../../components/images/icon_images/pic-2.jpg";
 import icon_3 from "../../components/images/icon_images/pic-3.jpg";
 import icon_4 from "../../components/images/icon_images/pic-4.jpg";
+import { IconButton } from "@mui/material";
 
 function Home() {
   const context = useContext(FoodContext);
@@ -26,6 +28,11 @@ function Home() {
           </h1>
           <p>we ensure quality on your every bite</p>
           <button onClick={() => navigate("/Pizza")}>order online</button>
+          <div className="arrow_down_cover">
+            <IconButton>
+                <DoubleArrowIcon className="arrow_down_cover_btn" />
+            </IconButton>
+          </div>
         </div>
       </div>
       <section className="offer_info">
